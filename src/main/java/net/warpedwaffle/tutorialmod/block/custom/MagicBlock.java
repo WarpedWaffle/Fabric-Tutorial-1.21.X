f3b9d966e7f8d616c950ad3025fd7adad5e97ac4
WarpedWaffle <milesdarmstrong@gmail.com>
2026-01-06 09:12:41 +0100

    custom tags tutorial

--- src/main/java/net/warpedwaffle/tutorialmod/block/custom/MagicBlock.java
+++ src/main/java/net/warpedwaffle/tutorialmod/block/custom/MagicBlock.java
@@ -17,6 +17,7 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.warpedwaffle.tutorialmod.item.ModItems;
+import net.warpedwaffle.tutorialmod.util.ModTags;
 
 import java.util.List;
 
@@ -37,11 +38,15 @@ public class MagicBlock extends Block {
     @Override
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
         if (entity instanceof ItemEntity itemEntity) {
-            if (itemEntity.getStack().getItem() == ModItems.RAW_PINK_GARNET) {
+            if (isValidItem(itemEntity.getStack())) {
                 itemEntity.setStack(new ItemStack(Items.DIAMOND, itemEntity.getStack().getCount()));
                 itemEntity.setToDefaultPickupDelay();
             }
         }
+    }
+
+    private boolean isValidItem(ItemStack stack) {
+        return stack.isIn(ModTags.Items.TRANSFORMABLE_ITEMS);
     }
 
     @Override
