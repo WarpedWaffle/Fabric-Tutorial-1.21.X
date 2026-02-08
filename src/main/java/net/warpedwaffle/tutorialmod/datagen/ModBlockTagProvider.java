465d0052759926fcc770a1d653a19fbd4459d1cd
WarpedWaffle <milesdarmstrong@gmail.com>
2026-02-08 18:34:18 +0100

    tools tutorial

--- src/main/java/net/warpedwaffle/tutorialmod/datagen/ModBlockTagProvider.java
+++ src/main/java/net/warpedwaffle/tutorialmod/datagen/ModBlockTagProvider.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.BlockTags;
 import net.warpedwaffle.tutorialmod.block.ModBlocks;
+import net.warpedwaffle.tutorialmod.util.ModTags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -27,5 +28,12 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
         getOrCreateTagBuilder(BlockTags.FENCES).add(ModBlocks.PINK_GARNET_FENCE);
         getOrCreateTagBuilder(BlockTags.FENCE_GATES).add(ModBlocks.PINK_GARNET_FENCE_GATE);
         getOrCreateTagBuilder(BlockTags.WALLS).add(ModBlocks.PINK_GARNET_WALL);
+
+        getOrCreateTagBuilder(BlockTags.NEEDS_DIAMOND_TOOL)
+                .add(ModBlocks.MAGIC_BLOCK);
+
+        getOrCreateTagBuilder(ModTags.Blocks.NEEDS_PINK_GARNET_TOOL)
+                .add(ModBlocks.MAGIC_BLOCK)
+                .addTag(BlockTags.NEEDS_IRON_TOOL);
     }
 }
--- src/main/java/net/warpedwaffle/tutorialmod/datagen/ModItemTagProvider.java
+++ src/main/java/net/warpedwaffle/tutorialmod/datagen/ModItemTagProvider.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.ItemTags;
 import net.warpedwaffle.tutorialmod.item.ModItems;
 import net.warpedwaffle.tutorialmod.util.ModTags;
 
@@ -22,5 +23,11 @@ public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
                 .add(Items.COAL)
                 .add(Items.STICK)
                 .add(Items.APPLE);
+
+        getOrCreateTagBuilder(ItemTags.SWORDS).add(ModItems.PINK_GARNET_SWORD);
+        getOrCreateTagBuilder(ItemTags.PICKAXES).add(ModItems.PINK_GARNET_PICKAXE);
+        getOrCreateTagBuilder(ItemTags.SHOVELS).add(ModItems.PINK_GARNET_SHOVEL);
+        getOrCreateTagBuilder(ItemTags.AXES).add(ModItems.PINK_GARNET_AXE);
+        getOrCreateTagBuilder(ItemTags.HOES).add(ModItems.PINK_GARNET_HOE);
     }
 }
